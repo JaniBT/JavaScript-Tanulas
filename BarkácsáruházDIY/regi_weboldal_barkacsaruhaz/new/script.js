f13cb5d4d57273eb281d4b5e191dc3d61d42359f
JaniBT <janoscsordas072@gmail.com>
2024-02-16 10:27:28 +0100

    lmao

---
+++ BarkácsáruházDIY/regi_weboldal_barkacsaruhaz/new/script.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("myNav").style.height = "100%";
+    document.querySelector(".carousel").style.visibility = "hidden";
+}
+
+function closeNav() {
+    document.getElementById("myNav").style.height = "0%";
+    document.querySelector(".carousel").style.visibility = "visible";
+}
